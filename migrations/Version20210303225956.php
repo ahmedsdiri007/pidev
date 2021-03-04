2bdad62e82a592b6249ec76b27de1ea6335d345b
leiilaa <48127465+leiilaa@users.noreply.github.com>
2021-03-04 11:14:21 +0100

    copie0 du crud
    copie0 du crud

---
+++ migrations/Version20210303225956.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210303225956 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE reservation CHANGE id_transport id_transport INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE transport CHANGE prise prise DATE DEFAULT \'CURRENT_TIMESTAMP\' NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE reservation CHANGE id_transport id_transport INT NOT NULL');
+        $this->addSql('ALTER TABLE transport CHANGE prise prise DATE NOT NULL');
+    }
+}
